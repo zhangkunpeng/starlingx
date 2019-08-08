c7d6d55ca8f3e6aa8467044d9895b99edcc1a86c
hutianhao6 <47875883+hutianhao6@users.noreply.github.com>
2019-08-08 14:55:18 +0800

    Update public_endpoint.sh

--- deployment/stx-container/deployment_yml/public_endpoint.sh
+++ deployment/stx-container/deployment_yml/public_endpoint.sh
@@ -1,27 +1,27 @@
 # Download yml files and update config.
 #!/bin/sh
 
-sudo wget -P /home/$USER/ https://github.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/cinder_override.yml
+sudo wget -P /home/$USER/ https://raw.githubusercontent.com/zhangkunpeng/starlingx/master/deployment/stx-container/deployment_yml/cinder_override.yml
 system helm-override-update stx-openstack cinder openstack --values cinder-override.yml
 
-sudo wget -P /home/$USER/ https://github.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/glance_override.yml
+sudo wget -P /home/$USER/ https://raw.githubusercontent.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/glance_override.yml
 system helm-override-update stx-openstack glance openstack --values glance-override.yml
 
-sudo wget -P /home/$USER/ https://github.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/heat_override.yml
+sudo wget -P /home/$USER/ https://raw.githubusercontent.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/heat_override.yml
 system helm-override-update stx-openstack heat openstack --values heat-override.yml
 
-sudo wget -P /home/$USER/ https://github.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/ironic_override.yml
+sudo wget -P /home/$USER/ https://raw.githubusercontent.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/ironic_override.yml
 system helm-override-update stx-openstack ironic openstack --values ironic-override.yml
 
-sudo wget -P /home/$USER/ https://github.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/keystone_override.yml
+sudo wget -P /home/$USER/ https://raw.githubusercontent.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/keystone_override.yml
 system helm-override-update stx-openstack keystone openstack --values keystone-override.yml
 
-sudo wget -P /home/$USER/ https://github.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/neutron_override.yml
+sudo wget -P /home/$USER/ https://raw.githubusercontent.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/neutron_override.yml
 system helm-override-update stx-openstack neutron openstack --values neutron-override.yml
 
-sudo wget -P /home/$USER/ https://github.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/nova_override.yml
+sudo wget -P /home/$USER/ https://raw.githubusercontent.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/nova_override.yml
 system helm-override-update stx-openstack nova openstack --values nova-override.yml
 
-sudo wget -P /home/$USER/ https://github.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/placement_override.yml
+sudo wget -P /home/$USER/ https://raw.githubusercontent.com/zhangkunpeng/starlingx/blob/master/deployment/stx-container/deployment_yml/placement_override.yml
 system helm-override-update stx-openstack placement openstack --values placement-override.yml
 

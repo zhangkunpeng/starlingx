113fc7f54309ee537d144aa1cff7c70740f93892
zhangkunpeng <zhang.kunpeng@99cloud.net>
2019-08-30 15:10:45 +0800

    update agent

--- application/openvino/agent/server.py
+++ application/openvino/agent/server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, World!"
-    
+
 @app.route('/openvino/<name>')
 def update_data(name):
     timer = threading.Timer(1, update_image, [name])
@@ -18,10 +18,10 @@ def update_image(name):
     conn = openstack.connect(cloud='central')
     for i in range(10):
         t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-        filename = name+"/"+str(t)+'.jpg'
-        obj = conn.create_object("openvino",filename, filename="demo.jpg")
+        filename = name+"/"+str(t)+'.png'
+        obj = conn.create_object("openvino",filename, filename="demo.png")
         print("upload file:"+filename)
         time.sleep(3)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8305)
+    app.run(debug=True,port=8305,host='0.0.0.0')

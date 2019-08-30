9602a0059271877070e22bceb3876c3f0f52594c
zhangkunpeng <zhang.kunpeng@99cloud.net>
2019-08-30 14:57:49 +0800

    add agent

---
+++ application/openvino/agent/server.py
@@ -0,0 +1,27 @@
+import openstack
+import os, time, threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+    
+@app.route('/openvino/<name>')
+def update_data(name):
+    timer = threading.Timer(1, update_image, [name])
+    timer.start()
+    return "ok"
+
+def update_image(name):
+    conn = openstack.connect(cloud='central')
+    for i in range(10):
+        t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        filename = name+"/"+str(t)+'.jpg'
+        obj = conn.create_object("openvino",filename, filename="demo.jpg")
+        print("upload file:"+filename)
+        time.sleep(3)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8305)
